0561fe6cd3640b81f8bbbea4e9a019b2b70d6f8d
sebastigurin <sebastigurin@gmail.com>
2012-04-25 17:56:02 +0000

    temp commented 1 test based on gwtjunit for proper compilation outside eclispe

--- raphael4gwt/src/org/sgx/raphael4gwt/raphael/jsutil/TestBase.java
+++ raphael4gwt/src/org/sgx/raphael4gwt/raphael/jsutil/TestBase.java
@@ -1,12 +1,12 @@
-package org.sgx.raphael4gwt.raphael.jsutil;
-
-import com.google.gwt.junit.client.GWTTestCase;
-
-public class TestBase extends GWTTestCase {
-
-	@Override
-	public String getModuleName() {
-		return "org.sgx.raphael4gwt.Raphael4Gwt";
-	}
-
-}
+//package org.sgx.raphael4gwt.raphael.jsutil;
+//
+//import com.google.gwt.junit.client.GWTTestCase;
+//
+//public class TestBase extends GWTTestCase {
+//
+//	@Override
+//	public String getModuleName() {
+//		return "org.sgx.raphael4gwt.Raphael4Gwt";
+//	}
+//
+//}

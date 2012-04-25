9cdba48259bb9ae8fea4373d8d34bfb3071cd10f
sebastigurin@gmail.com <sebastigurin@gmail.com@7e7bb6b8-4a40-46b3-c37c-876bc57a0d49>
2012-04-25 17:55:26 +0000

    fix on build.xml and userguide

--- trunk/raphael4gwt/doc/userguide/make-pdf.sh
+++ trunk/raphael4gwt/doc/userguide/make-pdf.sh
@@ -1,21 +1,7 @@
 #generation of htmls and pdf from docbook.
 #author: sgurin
 
-NAME=rm-user-guide
-#clean
-rm $NAME.pdf
-rm -r $NAME-htmls
-rm $NAME.tgz
+NAME=r4g-user-guide
 
-#pdf
 dblatex $NAME.xml
 
-#multiple htmls
-#db2html $NAME.xml
-#cp -r images $NAME/images
-#mv $NAME $NAME-htmls
-#cp $NAME-htmls/t2.html $NAME-htmls/index.html
-#chmod -R a+x $NAME-htmls
-
-tar cvfz $NAME-htmls.tgz $NAME-htmls
-#db2html --nochunks $NAME.xml > $NAME.html

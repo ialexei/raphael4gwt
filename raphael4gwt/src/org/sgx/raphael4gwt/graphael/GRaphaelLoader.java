19f3f76f914c6c3001afef4470a28c773f2b64ef
sebastigurin <sebastigurin@7e7bb6b8-4a40-46b3-c37c-876bc57a0d49>
2013-03-09 22:02:38 +0000

    fixed loader - noscript and maven pom introduced

---
+++ raphael4gwt/src/org/sgx/raphael4gwt/graphael/GRaphaelLoader.java
@@ -0,0 +1,60 @@
+package org.sgx.raphael4gwt.graphael;
+
+import org.sgx.raphael4gwt.raphael.RaphaelLoader;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.ScriptInjector;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ExternalTextResource;
+import com.google.gwt.resources.client.ResourceCallback;
+import com.google.gwt.resources.client.ResourceException;
+import com.google.gwt.resources.client.TextResource;
+
+/**
+ * an utility for loading raphaeljs async
+ * @author sg
+ *
+ */
+public class GRaphaelLoader {
+//	public static interface Listener {
+//		/**
+//		 * @param error if not null then it is the cause of an error and means that probably 
+//		 * raphael4gwt required script couln't be loaded
+//		 */
+//		void loaded(Exception error);
+//	}
+	public static interface RaphaelScripts extends ClientBundle {
+		public static RaphaelScripts instance = GWT.create(RaphaelScripts.class);
+
+		@Source("scripts/g.raphael.all-min.js")
+		ExternalTextResource graphael4gwtallminjs();
+	}
+	protected static boolean raphaelLoaded = false;
+	
+	public static void loadGRaphael(final RaphaelLoader.Listener l) {	
+		RaphaelLoader.loadRaphael(new RaphaelLoader.Listener() {			
+			@Override
+			public void loaded(Exception error) {
+				if(error!=null){
+					l.loaded(error); 
+				}
+				try {
+					RaphaelScripts.instance.graphael4gwtallminjs().getText(new ResourceCallback<TextResource>() {						
+						@Override
+						public void onSuccess(TextResource r) {
+							ScriptInjector.fromString(r.getText()).setWindow(ScriptInjector.TOP_WINDOW).inject();
+							l.loaded(null); 
+						}						
+						@Override
+						public void onError(ResourceException e) {
+							l.loaded(e); 
+						}
+					});
+				} catch (ResourceException e) {
+					l.loaded(e); 
+				} 
+			}
+		});
+	}
+
+}

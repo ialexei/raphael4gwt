86163ae8f4cdc4068114ead5cd0040242aaebb1a
sebastigurin <sebastigurin@7e7bb6b8-4a40-46b3-c37c-876bc57a0d49>
2013-03-09 22:03:04 +0000

    example fixed

--- raphael4gwttest/src/org/sgx/raphael4gwttest/client/Raphael4gwtNoScriptTestEP.java
+++ raphael4gwttest/src/org/sgx/raphael4gwttest/client/Raphael4gwtNoScriptTestEP.java
@@ -1,34 +1,44 @@
 package org.sgx.raphael4gwttest.client;
 
 import org.sgx.raphael4gwt.graphael.GPaper;
+import org.sgx.raphael4gwt.graphael.GRaphaelLoader;
 import org.sgx.raphael4gwt.graphael.pie.PieOpts;
 import org.sgx.raphael4gwt.raphael.Raphael;
-import org.sgx.raphael4gwt.raphael.RaphaelLoader;
 import org.sgx.raphael4gwt.raphael.RaphaelLoader.Listener;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class Raphael4gwtNoScriptTestEP implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		RaphaelLoader.loadGRaphael(new Listener() {			
+		RootPanel.get().add(new Button("load graphael example", new ClickHandler() {			
 			@Override
-			public void loaded(Exception error) {
-				if(error == null)
-					test1();
-				else {
-					Window.alert("ERROR loading graphael : "+error);
-					error.printStackTrace(); 
-				}
+			public void onClick(ClickEvent event) {
+				GRaphaelLoader.loadGRaphael(new Listener() {			
+					@Override
+					public void loaded(Exception error) {
+						if(error == null) {
+							doGraphaelExample(); 
+						}
+						else {
+							Window.alert("ERROR loading graphael : "+error);
+							error.printStackTrace(); 
+						}
+					}
+				}); 
 			}
-		}); 
-		test1();
+		})); 
+		
 	}
 
-	private void test1() {
-        GPaper paper = (GPaper)Raphael.paper(0,0,600,600);
+	private void doGraphaelExample() {
+        GPaper paper = Raphael.paper(0,0,600,600).cast();
         PieOpts opts = PieOpts.create(); 
         paper.piechart(200f, 200, 100, new double[]{10, 40, 70, 90}, opts);
 	}

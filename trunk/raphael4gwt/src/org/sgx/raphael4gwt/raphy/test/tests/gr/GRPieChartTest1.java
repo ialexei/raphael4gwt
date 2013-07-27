6b7ab15d6eb1e21ade951e8c136b6efa30b9d725
sebastigurin <sebastigurin@7e7bb6b8-4a40-46b3-c37c-876bc57a0d49>
2013-07-27 16:43:56 +0000

    raphy gr tests

---
+++ trunk/raphael4gwt/src/org/sgx/raphael4gwt/raphy/test/tests/gr/GRPieChartTest1.java
@@ -0,0 +1,52 @@
+package org.sgx.raphael4gwt.raphy.test.tests.gr;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.sgx.raphael4gwt.gtest.gallery.tests.PieChartTest2;
+import org.sgx.raphael4gwt.raphael.Paper;
+import org.sgx.raphael4gwt.raphael.Raphael;
+import org.sgx.raphael4gwt.raphy.client.Charts;
+import org.sgx.raphael4gwt.raphy.client.bar.Bar;
+import org.sgx.raphael4gwt.raphy.client.bar.BarChart;
+import org.sgx.raphael4gwt.raphy.client.bar.BarChartOpts;
+import org.sgx.raphael4gwt.raphy.client.bar.BarOpts;
+import org.sgx.raphael4gwt.raphy.client.line.Line;
+import org.sgx.raphael4gwt.raphy.client.line.LineChart;
+import org.sgx.raphael4gwt.raphy.client.line.LineChartOpts;
+import org.sgx.raphael4gwt.raphy.client.line.LineOpts;
+import org.sgx.raphael4gwt.raphy.client.line.Point;
+import org.sgx.raphael4gwt.raphy.test.app.AbstractRaphyTest;
+import org.sgx.raphael4gwt.raphy.test.tests.RaphyTestResources;
+
+import com.google.gwt.dom.client.DivElement;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.resources.client.ExternalTextResource;
+
+public class GRPieChartTest1 extends AbstractRaphyTest {
+
+	@Override
+	public void test(Element parent) {
+		int width=500, height=300;
+		DivElement e = Document.get().createDivElement();
+		parent.appendChild(e);
+		e.getStyle().setProperty("width", width+"px");
+		e.getStyle().setProperty("height", height+"px");
+		
+		Paper paper = Raphael.paper(e, width, height); 
+		org.sgx.raphael4gwt.gtest.gallery.tests.PieChartTest2 test = new PieChartTest2(paper, width, height);
+		test.test(); 
+	}
+
+	// test related information:
+	public GRPieChartTest1() {
+		super("GRPieChartTest1", "GRPieChartTest1");
+	}
+
+	public Map<String, ExternalTextResource> getResources() {
+		Map<String, ExternalTextResource> m = new HashMap<String, ExternalTextResource>();
+		m.put("PieChartTest2.java", RaphyTestResources.INSTANCE.GRPieChartTest2());
+		return m;
+	}
+}
